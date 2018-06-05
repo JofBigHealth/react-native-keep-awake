b1807b4b41f02dae49ed9e6979f1abd052f970d2
Jof <38071585+JofBigHealth@users.noreply.github.com>
2018-06-05 15:16:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,6 @@ import React, { Component } from 'react';
 import { NativeModules } from 'react-native';
 
 export default class KeepAwake extends Component<void, void> {
-  static activate() {
-    NativeModules.KCKeepAwake.activate();
-  }
-
-  static deactivate() {
-    NativeModules.KCKeepAwake.deactivate();
-  }
-
   componentWillMount() {
     KeepAwake.activate();
   }
